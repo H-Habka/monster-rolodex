251e176d60af257aecf1796f1bffeed531bf6878
H-Habka <hossien.habka1@gmail.com>
2022-02-11 08:44:29 +0200

    vedio 11 done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      name : "hossien",
+      age: "22"
+    }
+  }
   render(){
     return(
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello { this.state.name } your age is {this.state.age}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick= { () => {this.setState({
+          name: (this.state.name == "hasan")?"hossien": "hasan",
+          age:"25"
+          
+          })}}>Click to change text</button>
       </header>
     </div>
     );
